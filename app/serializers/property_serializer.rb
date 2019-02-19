c16ce039b355a846f6a646edcb8d24203be5f035
jessanderson222 <jessanderson222@gmail.com>
2019-02-19 18:27:01 -0500

    sends matches to propertys through serializer

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -1,4 +1,6 @@
 class PropertySerializer < ActiveModel::Serializer
-    attributes :id, :agent_id, :agent, :matches, :bedrooms, :bathrooms, :distance_to_subway, :borough, :neighborhood, :pet_friendly, :elevator, :laundry, :doorman, :move_in_date, :rent, :img_1, :img_2, :img_3
+    attributes :id, :agent_id, :agent, :matches, :renters, :bedrooms, :bathrooms, :distance_to_subway, :borough, :neighborhood, :pet_friendly, :elevator, :laundry, :doorman, :move_in_date, :rent, :img_1, :img_2, :img_3
     belongs_to :agent
+    has_many :matches
+    has_many :renters, through: :matches
 end 
